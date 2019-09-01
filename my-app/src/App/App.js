acdf126ccf2128a4c9180a93cb25d64da264bad8
Ola Bartnik <aleksandra.dadacz@gmail.com>
2019-09-01 17:01:28 +0200

    app config

---
+++ my-app/src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Button from "./Components/Button/button"
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+            <h1 className={"header"}>Hej!!</h1>
+            < Button />
+            </>)
+    }
+}
+
+export default App;
---
+++ my-app/src/App/Components/Button/button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./button.scss";
+
+
+class Button extends React.Component{
+     render() {return <button className={"button"}>Naciśnij mnie </button>
+     }
+}
+
+export default Button;
