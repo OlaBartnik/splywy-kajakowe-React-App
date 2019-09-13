fa9e950068e00566b46462384713374ef6d8420b
Ola Bartnik <aleksandra.dadacz@gmail.com>
2019-09-13 22:03:21 +0200

    wkra.js

--- my-app/src/App/Pages/State/Masovian/Wkra/wkra.js
+++ my-app/src/App/Pages/State/Masovian/Wkra/wkra.js
@@ -1,46 +1,68 @@
 import React from 'react';
 import "./wkra";
 
-// class Wkra extends React.Component{
-//     render() {return <h1>div</h1>
-//     }
-// }
-//
-// export default Wkra;
-
 
 class RouteSelection extends React.Component {
-
-    state = {
-        start: "Sobieski",
-        end: "Joniec"
+    constructor(props) {
+        super(props);
+        this.state = {
+            start: "Sobieski",
+            end: "Joniec",
+            distance: "",
+            duration: ""
+        }
     }
 
-    handleStartChange = e => {
-        this.setState({start: e.target.value})
+
+    handleChange = e => {
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({[name]: value})
     }
-    handleEndChange = e => {
-        this.setState({end: e.target.value})
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const {start} = this.state;
+        const {end} = this.state;
+        let distance = [];
+        let minDuration = [];
+        let maxDuration = [];
+        if (start === "Sochocin" && end === "Joniec") {
+            distance.push(17.5);
+            minDuration.push(4);
+            maxDuration.push(5);
+        }
+        this.setState({
+            distance: `${distance}km`,
+            duration: `${minDuration}-${maxDuration}h`
+        })
     }
 
     render() {
-        return (<form>
-                <label> Start spływu
-                    <select>
-                        <option value="Sochocin" onChange={this.handleStartChange}>Sochocin</option>
-                        <option value="Joniec" onChange={this.handleStartChange}>Joniec</option>
-                        <option value="Sobieski" onChange={this.handleStartChange}>Sobieski</option>
-                    </select>
-                </label>
-                <label> Koniec spływu
-                    <select>
-                        <option value="Bolęcin" onChange={this.handleEndChange}>Bolęcin</option>
-                        <option value="Sobieski" onChange={this.handleEndChange}>Sobieski</option>
-                        <option value="Joniec" onChange={this.handleEndChange}>Joniec</option>
-                    </select>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Start spływu
+                        <select name="start" value={this.state.start} onChange={this.handleChange}>
+                            <option value="Sochocin">Sochocin</option>
+                            <option value="Joniec">Joniec</option>
+                            <option value="Sobieski">Sobieski</option>
+                        </select>
+                    </label>
+                    <label> Koniec spływu
+                        <select name="end" value={this.state.end} onChange={this.handleChange}>
+                            <option value="Bolęcin">Bolęcin</option>
+                            <option value="Sobieski">Sobieski</option>
+                            <option value="Joniec">Joniec</option>
+                        </select>
+                    </label>
+                    <input type="submit" value="Wyznacz trasę"/>
+                </form>
+                <h3>Kilometry do przepłynięcia: {this.state.distance}</h3>
+                <h3>Średni czas spływu: {this.state.duration} </h3>
+            </div>
+
         )
     }
 }
--- my-app/src/App/Pages/State/Masovian/masovian.js
+++ my-app/src/App/Pages/State/Masovian/masovian.js
@@ -2,7 +2,11 @@ import React from 'react';
 import "./masovian.scss";
 
 class Masovian extends React.Component{
-    render() {return <div className={"masovianMap"}></div>
+    render() {return (<div className={"masovianMap"}></div>)
+        // (<div className=&quot;svn&quot; dangerouslySetInnerHTML={{ __html: }}/>)
+
+
+
     }
 }
 
