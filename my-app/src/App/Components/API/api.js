a83abbb451666c761df777ffd9d759a5c4639a4d
Ola Bartnik <aleksandra.dadacz@gmail.com>
2019-09-15 11:31:04 +0200

    API done

--- my-app/src/App/Components/API/api.js
+++ my-app/src/App/Components/API/api.js
@@ -8,8 +8,53 @@ class API extends React.Component {
         super(props);
         this.state = {
             city: "",
+            id: "",
             error: false
+        };
+
+        this.setNewJs = this.setNewJs.bind(this);
+    }
+
+    setNewJs(cityid) {
+        let script = this.newHeaderScript(cityid);
+        let scriptbox = this.newWiget();
+
+        if(document.querySelector('#pluginWeader')){
+            document.querySelector('#pluginWeader').replaceWith(script);
+        } else {
+            document.body.appendChild(script);
         }
+
+        if(document.querySelector('#pluginWeaderBox')){
+            document.querySelector('#pluginWeaderBox').replaceWith(scriptbox);
+        } else {
+            document.body.appendChild(scriptbox);
+        }
+    }
+
+
+    newHeaderScript(cityid) {
+        var script = document.createElement('script');
+        script.id = "pluginWeader";
+        script.innerHTML = `
+        window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+        window.myWidgetParam.push({
+            id: 15,
+            cityid: '${cityid}',
+            appid: '3d6df3bc290f22a1f90a6f7ab363e060',
+            units: 'metric',
+            containerid: 'openweathermap-widget-15'
+        });`;
+        return script;
+    }
+
+    newWiget() {
+        var script = document.createElement('script');
+        script.id = "pluginWeaderBox";
+        script.async = true;
+        script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+        return script;
     }
 
     componentDidMount() {
@@ -25,16 +70,20 @@ class API extends React.Component {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                //this.setState({error: false, city: data.name})
+                this.setState({error: false, city: data.name, id: data.id})
             })
             .catch(err => {
                 console.log(err);
                 this.setState({error: true})
-            })
+            });
 
+        this.setNewJs(770028);
     }
 
-    render() {return (<h1>testeeste</h1>)
+    render() {
+        return (<div>
+            <div id="openweathermap-widget-15"></div>
+        </div>)
     }
 }
 
